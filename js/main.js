5c958a4933f9fec91250be90ce548aa862620b04
Hleb <103289584+Glebiko@users.noreply.github.com>
2025-09-21 19:24:13 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// const swiper = new Swiper(".shopline__slider", {
+//   slidesPerView: 4,
+//   spaceBetween: 20,
+//   freeMode: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+// });
+
+const swiper = new Swiper(".shopline__slider", {
+  // pagination: {
+  //   el: ".swiper-pagination",
+  //   dynamicBullets: true,
+  // },
+});
